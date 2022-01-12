a2b0188dca7140d03d78f17484794ce96b12be81
wainer <wainer@wbcsistemas.com.br>
2022-01-11 22:01:10 -0200

    exemplo de soma em duas paginas

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somador</title>
+</head>
+<body>
+    <form action='soma.php' method='POST'>
+      <input name='num1' type='number' placeholder=' Numero 1'>
+      <input name='num2' type='number' placeholder=' Numero 2'>
+      <button type='submit'>Somar</button>
+    </form>
+</body>
+
+</html>
---
+++ soma.php
@@ -0,0 +1,20 @@
+<?php
+
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$soma = $num1 + $num2;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>somador</title>
+</head>
+<body>
+    <h1><?php echo $soma ?>
+
+</body>
+</html>
